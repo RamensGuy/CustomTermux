93d7eb787f0a254300eae6b1490e0acbc59af980
RamensGuy <108737094+RamensGuy@users.noreply.github.com>
2022-07-11 19:00:34 +0200

    Update FreeCustomTermux.sh

--- FreeCustomTermux.sh
+++ FreeCustomTermux.sh
@@ -1,5 +1,7 @@
 clear
 echo -e "\e[32mWelcome To my Custom Termux Have Fun !! :)\e[0m";
-pkg  uninstall ncurses
+echo -e "\e[32mWelcome To my Custom Termux Have Fun !! :)\e[0m";
+echo -e "\e[32mWelcome To my Custom Termux Have Fun !! :)\e[0m";
+echo -e "\e[32mWelcome To my Custom Termux Have Fun !! :)\e[0m";
 pkg  uninstall coreutils
 pkg  uninstall termux-tools
